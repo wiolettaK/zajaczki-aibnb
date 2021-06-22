a7250e1d4a37452d5f617655bfb8aa4575ebd724
wiolettaK <kotarskawioletta1@gmail.com>
2021-06-22 11:17:35 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from 'react';
+
+export const App: FunctionComponent = (): JSX.Element => {
+ 
+    return (
+        <div>Hello React App from scratch!!</div>
+    );
+ };
+ export default App;
