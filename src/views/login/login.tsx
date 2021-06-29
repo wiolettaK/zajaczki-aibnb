a18cbea6b04d003aedaa856e097e235b3f182410
wiolettaK <kotarskawioletta1@gmail.com>
2021-06-29 08:57:28 +0200

    COMMIT MESSAGES TEMPLATE: If applied, this commit will add validation

--- src/views/login/login.tsx
+++ src/views/login/login.tsx
@@ -1,114 +1,127 @@
-import { Avatar, Box, Button, Checkbox, Container, CssBaseline, FormControlLabel, Grid, makeStyles, TextField, Typography, withStyles } from '@material-ui/core';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Link from '@material-ui/core/Link';
-import React, { FunctionComponent, SyntheticEvent, useState } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-import './login.scss';
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  makeStyles,
+  TextField,
+  Typography,
+  withStyles,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+//import Link from "@material-ui/core/Link";
+import React, { FunctionComponent, SyntheticEvent, useState } from "react";
+import { Link } from "react-router-dom";
+import "./login.scss";
+import colors from "styles/variables.module.scss";
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%',
-      marginTop: theme.spacing(1),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: colors.secondary,
+  },
 }));
 
 const CssTextField = withStyles({
-    root: {
-      '& label.Mui-focused': {
-        color: 'green',
-      },
-      '& .MuiOutlinedInput-root': {
-
-        '&.Mui-focused fieldset': {
-          borderColor: 'green',
-        },
+  root: {
+    "& label.Mui-focused": {
+      color: colors.secondary,
+    },
+    "& .MuiOutlinedInput-root": {
+      "&.Mui-focused fieldset": {
+        borderColor: colors.secondary,
       },
     },
-  })(TextField);
+  },
+})(TextField);
 
 const Login: FunctionComponent<any> = (): JSX.Element => {
+  const classes = useStyles();
 
-    const classes = useStyles();
+  const submitForm = (e: React.FormEvent): void => {
+    console.log("Siemaneczko");
+  };
 
-    const submitForm = (e: React.FormEvent): void => {
-        console.log('Siemaneczko');
-     };
-
-    return (
-        <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Zaloguj
-          </Typography>
-          <form className={classes.form} onSubmit={submitForm} noValidate>
-            <CssTextField 
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Adres email użytkownika"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Hasło"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            {/* <FormControlLabel
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Zaloguj
+        </Typography>
+        <form className={classes.form} onSubmit={submitForm} noValidate>
+          <CssTextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Adres email użytkownika"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <CssTextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Hasło"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
             /> */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={`login-button ${classes.submit}`}
-            >
-              Zaloguj się
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Zapomniałeś hasła?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Nie masz konta? Zarejestruj się!"}
-                </Link>
-              </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={`login-button ${classes.submit}`}
+          >
+            Zaloguj się
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link to="/login" href="#" style={{ color: colors.secondary }}>
+                Zapomniałeś hasła?
+              </Link>
             </Grid>
-          </form>
-        </div>
-      </Container>
-    );
- };
- 
- export default Login;
+            <Grid item>
+              <Link to="/register" href="#" style={{ color: colors.secondary }}>
+                {"Nie masz konta? Zarejestruj się!"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default Login;
--- src/views/register/register.tsx
+++ src/views/register/register.tsx
@@ -1,37 +1,150 @@
-import { Avatar, Box, Button, Checkbox, Container, CssBaseline, FormControlLabel, Grid, Link, makeStyles, TextField, Typography } from '@material-ui/core';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import React, { FunctionComponent, SyntheticEvent, useState } from 'react';
-import {RouteComponentProps } from 'react-router-dom';
-  
+import {
+  Avatar,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  //Link,
+  makeStyles,
+  TextField,
+  Typography,
+  withStyles,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import React, { FunctionComponent, useState } from "react";
+import { Link } from "react-router-dom";
+import colors from "styles/variables.module.scss";
+
 const useStyles = makeStyles((theme) => ({
-    paper: {
-      marginTop: theme.spacing(8),
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-    },
-    avatar: {
-      margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-      width: '100%', // Fix IE 11 issue.
-      marginTop: theme.spacing(3),
-    },
-    submit: {
-      margin: theme.spacing(3, 0, 2),
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", 
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: colors.secondary,
+  },
 }));
 
+const CssTextField = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: colors.secondary,
+    },
+    "& .MuiOutlinedInput-root": {
+      "&.Mui-focused fieldset": {
+        borderColor: colors.secondary,
+      },
+    },
+  },
+})(TextField);
+
 const Register: FunctionComponent<any> = (): JSX.Element => {
+  const classes = useStyles();
+  const validEmailRegex = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+[.][a-z]{2,}$/);
 
-    const classes = useStyles();
-    const handleSubmit = (e: React.FormEvent): void => {
-        console.log('Siemaneczko' , e);
-     };
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    showPassword: false,
+    errors: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      lastName: "",
+      firstName: "",
+    },
+  });
+  const validateFiltr = (name: string, value: string): void => {
+    let error;
 
+    switch (name) {
+      case "email":
+        error = validEmailRegex.test(value)
+          ? ""
+          : "Wpisz poprawny adres email.";
+        break;
+      case "password":
+        error =
+          value.length < 8 ? "Zbyt krótkie hasło (minimum 8 znaków)." : "";
+        break;
+      case "confirmPassword":
+        console.log(values.password, values.confirmPassword);
+        error =
+          value.length < 8
+            ? "Zbyt krótkie hasło (minimum 8 znaków)."
+            : (error =
+                values.password &&
+                values.confirmPassword &&
+                values.password === values.confirmPassword
+                  ? ""
+                  : "Hasła nie są takie same. Spróbuj ponownie.");
+        break;
+      case "firstName":
+      case "lastName":
+        error = value.length < 1 ? "Wpisz imię i nazwisko." : "";
+        break;
+      default:
+        error = "";
+        break;
+    }
+
+    setValues({
+      ...values,
+      [name]: value,
+      errors: { ...values.errors, [name]: error },
+    });
+    console.log(values.errors);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    validateFiltr(name, value);
+  };
+
+  const validateForm = (): boolean => {
+    const { firstName, lastName, email, password, confirmPassword } = values;
     return (
-        <Container component="main" maxWidth="xs">
+      firstName.length > 0 &&
+      lastName.length > 0 &&
+      validEmailRegex.test(email) &&
+      password.length > 7 &&
+      confirmPassword.length > 7 &&
+      password === confirmPassword
+    );
+  };
+  const submitForm = (e: React.FormEvent): void => {
+    const { firstName, lastName, email, password, confirmPassword } = values;
+    e.preventDefault();
+    if (validateForm()){
+       alert("Zarejestrowano");
+     }else{
+      validateFiltr("firstName", firstName);
+      console.log('gg', firstName);
+      validateFiltr("lastName", lastName);
+      validateFiltr("email", email);
+      console.log(values.errors);
+      validateFiltr("password", password);
+ 
+      console.log(values.errors);
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
@@ -40,10 +153,10 @@ const Register: FunctionComponent<any> = (): JSX.Element => {
         <Typography component="h1" variant="h5">
           Zarejestruj się
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={submitForm} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
-              <TextField
+              <CssTextField
                 autoComplete="fname"
                 name="firstName"
                 variant="outlined"
@@ -52,10 +165,14 @@ const Register: FunctionComponent<any> = (): JSX.Element => {
                 id="firstName"
                 label="Imię"
                 autoFocus
+                value={values.firstName}
+                onChange={handleChange}
+                helperText={values.errors.firstName}
+                error={!!values.errors.firstName}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField
+              <CssTextField
                 variant="outlined"
                 required
                 fullWidth
@@ -63,10 +180,14 @@ const Register: FunctionComponent<any> = (): JSX.Element => {
                 label="Nazwisko"
                 name="lastName"
                 autoComplete="lname"
+                value={values.lastName}
+                onChange={handleChange}
+                helperText={values.errors.lastName}
+                error={!!values.errors.lastName}
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField
+              <CssTextField
                 variant="outlined"
                 required
                 fullWidth
@@ -74,10 +195,14 @@ const Register: FunctionComponent<any> = (): JSX.Element => {
                 label="Adres email"
                 name="email"
                 autoComplete="email"
+                value={values.email}
+                onChange={handleChange}
+                helperText={values.errors.email}
+                error={!!values.errors.email}
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField
+              <CssTextField
                 variant="outlined"
                 required
                 fullWidth
@@ -85,19 +210,25 @@ const Register: FunctionComponent<any> = (): JSX.Element => {
                 label="Hasło"
                 type="password"
                 id="password"
-                autoComplete="current-password"
+                value={values.password}
+                onChange={handleChange}
+                helperText={values.errors.password}
+                error={!!values.errors.password}
               />
             </Grid>
             <Grid item xs={12}>
-              <TextField
+              <CssTextField
                 variant="outlined"
                 required
                 fullWidth
-                name="repeatPassword"
+                name="confirmPassword"
                 label="Powtórz hasło"
-                type="repeatPassword"
-                id="repeatPassword"
-                autoComplete="repeat-password"
+                type="password"
+                id="confirmPassword"
+                value={values.confirmPassword}
+                onChange={handleChange}
+                helperText={values.errors.confirmPassword}
+                error={!!values.errors.confirmPassword}
               />
             </Grid>
           </Grid>
@@ -108,11 +239,11 @@ const Register: FunctionComponent<any> = (): JSX.Element => {
             color="primary"
             className={classes.submit}
           >
-            Zarejestruj się    
+            Zarejestruj się
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link to="/login" href="#" style={{ color: colors.secondary }}>
                 Masz już konto? Zaloguj się!
               </Link>
             </Grid>
@@ -120,7 +251,7 @@ const Register: FunctionComponent<any> = (): JSX.Element => {
         </form>
       </div>
     </Container>
-    );
- };
- 
- export default Register;
+  );
+};
+
+export default Register;
